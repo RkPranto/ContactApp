770db7c205ad9285f6d126e3e97ffa57b7edbcbe
Rezwan Pranto <rezwan2525@gmail.com>
2019-03-03 23:40:46 +0600

    Firebase data read and write implementation

--- app/src/main/java/com/example/pranto/contactapp/MainActivity.java
+++ app/src/main/java/com/example/pranto/contactapp/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.pranto.contactapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText name, phone;
+    Button submit, load;
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.username);
+        phone = findViewById(R.id.phone_number);
+        submit = findViewById(R.id.submit);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String n = name.getText().toString().trim(), i = phone.getText().toString().trim();
+                String key = databaseReference.push().getKey();
+                Student s = new Student(n, i);
+                Map<String, Object> stud = s.toMap();
+                databaseReference.child(key).setValue(stud);
+            }
+        });
+
+        load = findViewById(R.id.load_data);
+        load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Details.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
+
+
+
 }
